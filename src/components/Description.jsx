6cbbc7efc4d8c3e09b6ccd19d2f2ecff1bcaeb7d
mfrrh <125086050+meriemFERRAH@users.noreply.github.com>
2023-10-15 20:48:37 +0100

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 function Description() {
   const [showDescription1, setShowDescription1] = useState(true);
@@ -10,23 +10,23 @@ function Description() {
     <div className='description'>
       <div className="values">
         <div className="item">
-             <img src="./src/icons/icons8-creative-64.png" alt="" />
+             <img src="src/icons/icons8-creative-64.png" alt="" />
             <p>CREATIVE THINKING</p>
         </div>
         <div className="item">
-        <img src="./src/icons/icons8-idea-48.png" alt="" />
+        <img src="src/icons/icons8-idea-48.png" alt="" />
             <p>INSPIRING IDEAS</p>
             </div>
             <div className="item">
-            <img src="./src/icons/icons8-innovative-96.png" alt="" />
+            <img src="src/icons/icons8-innovative-96.png" alt="" />
             <p>INNOVATIVE IDEAS</p>
             </div>
             <div className="item">
-            <img src="./src/icons/icons8-medal-100.png" alt="" />
+            <img src="src/icons/icons8-medal-100.png" alt="" />
             <p>BEST QUALITY</p>
             </div>
             <div className="item">
-            <img src="./src/icons/icons8-team-96.png" alt="" />
+            <img src="src/icons/icons8-team-96.png" alt="" />
             <p>TEAM SPIRIT</p>
             </div>
       </div>
@@ -35,11 +35,11 @@ function Description() {
        <span className={showDescription1 ? "hide" : "spandes2"}>Workstations hum with activity, Adorned with an array of circuitry, while the air crackles with the thrill of discovery. Yet, Beyond the gadgets and code, <span className="bold">Space Tech</span>  is a community built on mutual support and celebration. Through outreach and education, its impact extends far beyond its walls, igniting a passion for technology in the hearts of the next generation.</span>
       </div>
        <div className="arrow">
-       <img  src="./src/slide/arrow-right.png"
+       <img  src="src/slide/arrow-right.png"
           alt="arrow-right.png"
           onClick={toggleDescription}
           className={showDescription1 ? "right" : "left hide"}/>
-        <img src="./src/slide/arrow-left.png"
+        <img src="src/slide/arrow-left.png"
           alt="arrow-left.png"
           onClick={toggleDescription}
           className={showDescription1 ? "left hide" : "right"} />
